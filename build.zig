96082aab7f5b4ecaa71187f887c19c54859a7e18
Donkopakad <119570511+Donkopakad@users.noreply.github.com>
2026-02-10 15:51:47 +0530

    Refactor CUDA build process and add C source files

--- build.zig
+++ build.zig
@@ -14,23 +14,37 @@ pub fn build(b: *std.Build) void {
     });
 
     exe.root_module.addImport("websocket", websocket_dep.module("websocket"));
-
-    exe.addIncludePath(b.path("src/cuda/"));
-    exe.addLibraryPath(.{ .cwd_relative = "/opt/cuda/lib64" });
     exe.linkLibC();
-    exe.linkSystemLibrary("cudart");
-    exe.addObjectFile(b.path("kernel.o"));
 
-    exe.addObjectFile(b.path("simd.o"));
+    exe.addCSourceFile(.{ .file = b.path("src/signal_engine/simd.c"), .flags = &.{ "-mavx2" } });
+
+    const nvcc = b.findProgram(&.{"nvcc"}, &.{}) catch null;
+    if (nvcc) |nvcc_path| {
+        const nvcc_cmd = b.addSystemCommand(&.{
+            nvcc_path,
+            "-c",
+            "-O2",
+            "-arch=sm_75",
+            "-I",
+            "src/stat_calc",
+            "src/stat_calc/kernel.cu",
+            "-o",
+        });
+        const cuda_obj = nvcc_cmd.addOutputFileArg("kernel_cuda.o");
+        exe.step.dependOn(&nvcc_cmd.step);
+        exe.addObjectFile(cuda_obj);
+        exe.linkSystemLibrary("cudart");
+        exe.addCSourceFile(.{ .file = b.path("src/stat_calc/cuda_stub.c"), .flags = &.{ "-DENABLE_CUDA_BACKEND=1" } });
+    } else {
+        exe.addCSourceFile(.{ .file = b.path("src/stat_calc/cuda_stub.c"), .flags = &.{} });
+    }
 
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
 
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
+    if (b.args) |args| run_cmd.addArgs(args);
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
